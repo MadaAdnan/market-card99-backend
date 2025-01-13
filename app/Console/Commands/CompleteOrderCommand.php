3742e117104166ee316933e39e7f99b9e9c3e27d
adnan <mh.shamey@gmail.com>
2025-01-13 09:56:33 +0300

    fix report date

--- app/Console/Commands/CompleteOrderCommand.php
+++ app/Console/Commands/CompleteOrderCommand.php
@@ -41,6 +41,7 @@ class CompleteOrderCommand extends Command
     {
         $orders = Bill::where('status', \App\Enums\BillStatusEnum::PENDING->value)->whereNotNull('api_id')->get();
         $setting = Setting::first();
+        info("TEST ORDER -".$orders->count());
         foreach ($orders as $order) {
             info("TEST ORDER");
             switch ($order->api) {
