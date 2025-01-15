0457137f78b8019da0bbcfb9d607f22a64aa41b8
adnan <mh.shamey@gmail.com>
2025-01-15 21:56:32 +0300

    add juneed api

--- app/FromApi/Juneed.php
+++ app/FromApi/Juneed.php
@@ -30,7 +30,8 @@ class Juneed implements PayByApi
             $response = \Http::withHeaders([
                 'api-token' => $token
             ])->get($url);
-
+            info('JUNEED ');
+            info($response->body());
             sleep(1);
             if ($response->successful() && \Str::lower($response->json('status')) == 'ok' && $response->json('data')['status'] != 'not_available') {
                 $bill->api = $bill->product->api;
@@ -59,6 +60,8 @@ class Juneed implements PayByApi
             $response = \Http::withHeaders([
                 'api-token' => $token
             ])->get($url);
+            info('JUNEED ');
+            info($response->body());
             sleep(1);
             if (($response->successful() && \Str::lower($response->json('status')) == 'ok') || $response->json('data')['status'] != 'not_available') {
                 $bill->api = $bill->product->api;
