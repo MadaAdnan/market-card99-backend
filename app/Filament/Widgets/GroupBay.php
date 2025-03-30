8e9dcd923020c34b6da105ef61fe2e9c4fd59b80
sham <albasha.sq@gmail.com>
2025-03-30 12:55:00 +0300

    add syr coins

--- app/Filament/Widgets/GroupBay.php
+++ app/Filament/Widgets/GroupBay.php
@@ -57,9 +57,11 @@ class GroupBay extends ApexChartWidget
     {
         //$bill = Bill::orWhere('status', [BillStatusEnum::COMPLETE->value, BillStatusEnum::SUCCESS->value])->whereBetween('created_at', [Carbon::parse($this->filterFormData['date_start']), Carbon::parse($this->filterFormData['date_end'])])->sum('price');
 
-        $trend = Trend::query(Order::where('status', OrderStatusEnum::COMPLETE->value)->when($this->filterFormData['group'],fn($q)=>$q->whereHas('user',fn($query)=>$query
-            ->whereHas('group',fn($q)=>$q->where('groups.id',$this->filterFormData['group']))
-        ))
+        $trend = Trend::query(Order::where('status', OrderStatusEnum::COMPLETE->value)
+            ->when($this->filterFormData['group'], function ($query) {
+                $groupId = $this->filterFormData['group'];
+                $query->whereHas('user.group', fn($q) => $q->where('groups.id', $groupId));
+            })
     )
             ->between(
                 start: now()->startOfYear(),
